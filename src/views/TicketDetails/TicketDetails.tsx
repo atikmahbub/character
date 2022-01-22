79b3fb56eb759012637660e5e12a717d582451ec
Atik Mahbub <atikmahbub@Atiks-MacBook-Air.local>
2022-01-23 01:35:21 +0600

    added media query

--- src/views/TicketDetails/TicketDetails.tsx
+++ src/views/TicketDetails/TicketDetails.tsx
@@ -11,6 +11,7 @@ import {
   GET_CHARACTERS_SUCCESS,
   GET_CHARACTERS_ERROR,
 } from "../../reducers/characterReducer";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const StyledImage = styled("img")({
   borderRadius: 5,
@@ -25,6 +26,7 @@ const DoubleColumn = styled(Box)({
 const TicketDetails = () => {
   const { id } = useParams();
   const [state, dispatch] = useReducer(characterReducer, initialState);
+  const matches = useMediaQuery("(max-width:768px)");
 
   useEffect(() => {
     dispatch({ type: GET_CHARACTERS_PENDING });
@@ -46,7 +48,13 @@ const TicketDetails = () => {
   return (
     <Fragment>
       <AddTitle title="Character Details" backButton />
-      <Box display="flex" gap={2} alignItems="flex-start" mt={10}>
+      <Box
+        display="flex"
+        flexDirection={matches ? "column" : "row"}
+        gap={2}
+        alignItems="flex-start"
+        mt={10}
+      >
         <StyledImage src={state.data?.image} alt="pic" width={350} />
         <Stack spacing={1}>
           <Typography fontWeight={700} variant="h4">
